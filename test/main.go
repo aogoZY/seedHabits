6f31a92af34497060e0d77485551e1b2e9a7a281
周阳 <zhouyang@zhouyangdeMacBook-Pro.local>
2020-03-04 22:35:08 +0800

    fix 跨域

--- test/main.go
+++ test/main.go
@@ -8,12 +8,40 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/go-xorm/xorm"
 	"log"
+	"net/http"
+
 )
 
 
 
+// 处理跨域请求,支持options访问
+func Cors() gin.HandlerFunc {
+   return func(c *gin.Context) {
+      method := c.Request.Method
+      fmt.Println(method)
+      c.Header("Access-Control-Allow-Origin", "*")
+      c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token")
+      c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, PATCH, DELETE")
+      c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
+      c.Header("Access-Control-Allow-Credentials", "true")
+
+      // 放行所有OPTIONS方法，因为有的模板是要请求两次的
+      if method == "OPTIONS" {
+         c.AbortWithStatus(http.StatusNoContent)
+      }
+
+      // 处理请求
+      c.Next()
+   }
+}
+
+
 func main() {
-	r := gin.Default()
+	// r := gin.Default()
+
+	r := gin.New()
+	r.Use(Cors())
+
 	r.GET("/", func(c *gin.Context) {
 		db,err:=connectPgDB()
 		if err!=nil{
